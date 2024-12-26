ac9d26f60934bac1550f350a7d5fd36f8a231234
samat <samhardy1927@gmail.com>
2024-12-26 18:32:04 +0300

    Test2 changes

--- src/test/java/com/beganov/vendorservice/controller/VendorControllerTest.java
+++ src/test/java/com/beganov/vendorservice/controller/VendorControllerTest.java
@@ -2,16 +2,11 @@ package com.beganov.vendorservice.controller;
 
 import com.beganov.vendorservice.dto.VendorResponse;
 import com.beganov.vendorservice.service.VendorService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
