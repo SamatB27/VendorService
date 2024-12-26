1a5277acc494c767daa7620877e29472fa072140
samat <samhardy1927@gmail.com>
2024-12-26 11:42:01 +0300

    Vendor

--- src/main/java/com/beganov/vendorservice/model/Vendor.java
+++ src/main/java/com/beganov/vendorservice/model/Vendor.java
@@ -1,4 +1,19 @@
 package com.beganov.vendorservice.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
 public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String product;
+    private String description;
+
 }
